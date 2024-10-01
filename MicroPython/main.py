aa2aedf7a15fb7a61f984c944ce4996ef5e7c88c
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-01 15:28:16 +0000

    added flowchart & added cookie clicker python code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,27 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Peter Zerbinos
+Created on: Oct 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+sleep(400)
+cookieNumber = 0
+
+#Click cookie
+display.clear
+while True:
+    if button_a.was_pressed():
+        cookieNumber = cookieNumber + 1
+        display.show(cookieNumber)
+
+#Clear cookie
+display.clear
+while True:
+    if button_b.was_pressed():
+        cookieNumber = 0
+        display.show(cookieNumber)
